6bef548469b6ef9d6c4faa30eabf1e20ed6efb71
prahladinala <prahlad2018@gmail.com>
2021-05-06 23:50:58 +0530

    INitial Commit

--- App.js
+++ App.js
@@ -3,9 +3,10 @@ import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+  console.log("APP EXECUTED")
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>https://prahladinala.com</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -14,7 +15,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#f15025',
     alignItems: 'center',
     justifyContent: 'center',
   },
